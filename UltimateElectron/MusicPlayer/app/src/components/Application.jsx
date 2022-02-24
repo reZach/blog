6a51ead368e599c58f0d080c4dceafd2d23b6f9f
reZach <reZach@users.noreply.github.com>
2022-02-23 23:01:11 -0600

    making a new component to fit with the blogpost

--- UltimateElectron/MusicPlayer/app/src/components/Application.jsx
+++ UltimateElectron/MusicPlayer/app/src/components/Application.jsx
@@ -1,32 +1,28 @@
 import React from "react";
 import AlbumArt from "./AlbumArt/AlbumArt.jsx";
 import SongList from "./SongList.jsx";
+import Controls from "./Controls.jsx";
 
 class Application extends React.Component {
-    render(){
+    render() {
         return (
-            <div className="bg-red-200">
-                {SongList()}
-                {AlbumArt("https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png")}
+            <div className="h-full">
+                <div className="h-4/5 grid grid-cols-12 grid-rows-1 gap-0">
+                    <div className="col-span-3 bg-gray-300 border-black border-l-2 border-t-2">
+                        {SongList()}
+                    </div>
+                    <div className="col-span-9 border-black border-l-2 border-t-2 border-r-2">
+                        {AlbumArt(
+                            "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+                        )}
+                    </div>
+                </div>
+                <div className="h-1/5 bg-gray-400 border-black border-2">
+                    {Controls()}
+                </div>
             </div>
         );
     }
-
-    // render() {
-    //     return (
-    //         <div className="h-full">
-    //             <div className="h-4/5 grid grid-cols-12 grid-rows-1 gap-0">
-    //                 <div className="col-span-3">{SongList()}</div>
-    //                 <div className="col-span-9">
-    //                     {AlbumArt(
-    //                         "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
-    //                     )}
-    //                 </div>
-    //             </div>
-    //             <div className="h-1/5 bg-red-400">{/* render controls */}</div>
-    //         </div>
-    //     );
-    // }
 }
 
 export default Application;
---
+++ UltimateElectron/MusicPlayer/app/src/components/Controls.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Controls(props) {
+    return (
+        <div className="h-full grid grid-cols-1 grid-rows-2 justify-items-center">
+            <div className="self-center grid grid-cols-3 gap-x-2">
+                <button className="bg-blue-500 text-white rounded py-2 px-4">Previous</button>
+                <button className="bg-blue-500 text-white rounded py-2 px-4">Play</button>
+                <button className="bg-blue-500 text-white rounded py-2 px-4">Next</button>
+            </div>
+            <div className="self-center">
+                Volume <input type="range" min="0" max="100" />
+            </div>
+        </div>
+    );
+}
+
+export default Controls;
